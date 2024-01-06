c6ddd91572449afe37f22d795f48b76dafee25ef
mNik033 <nktps40@gmail.com>
2024-01-06 10:04:30 +0530

    defconfig, msm_isp_axi_util and build.sh changes

--- drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp_axi_util.c
@@ -851,8 +851,6 @@ static void msm_isp_sync_dual_cam_frame_id(
 		}
 	}
 	/* the number of frames that are dropped */
-	vfe_dev->isp_page->dual_cam_drop =
-				frame_id - (src_info->frame_id + 1);
 	ms_res->active_src_mask |= (1 << src_info->dual_hw_ms_info.index);
 	src_info->frame_id = frame_id;
 	src_info->dual_hw_ms_info.sync_state = MSM_ISP_DUAL_CAM_SYNC;
@@ -891,7 +889,6 @@ void msm_isp_increment_frame_id(struct vfe_device *vfe_dev,
 				pr_err_ratelimited("Frame out of sync on vfe %d\n",
 					vfe_dev->pdev->id);
 				/* Notify to do reconfig at SW sync drop*/
-				vfe_dev->isp_page->dual_cam_drop_detected = 1;
 				/*
 				 * set this isp as async mode to force
 				 *it sync again at the next sof
